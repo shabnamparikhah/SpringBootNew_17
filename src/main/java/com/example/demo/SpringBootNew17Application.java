9d4a75c17f9f0429a89a4d68f6fe0654a8de89ab
GBTC441012UR <GBTC441012UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-08-13 10:05:44 -0400

    Initial

---
+++ src/main/java/com/example/demo/SpringBootNew17Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNew17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNew17Application.class, args);
+	}
+}
